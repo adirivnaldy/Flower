da85fbfa9841f27d92b1193bb221e942ba5c7117
adirivnaldy <adirivnaldy@gmail.com>
2025-03-04 15:37:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-onload = () =>{
-        document.body.classList.remove("container");
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+
+    const titles = ('Happy Birthday').split('')
+    const titleElement = document.getElementById('title');
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 300); // 1000ms delay
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
 };
